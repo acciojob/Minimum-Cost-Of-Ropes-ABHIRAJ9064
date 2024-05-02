ebf44efcfc74915df23580470f06535de8ef0f0f
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-05-02 12:57:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function calculateMinCost() {
   //your code here
-  
+  import heapq
+def ConnectRope(ropes):
+    heapq.heapify(ropes)
+    res = 0
+    while len(ropes) > 1:
+        cur = heapq.heappop(ropes)+heapq.heappop(ropes)
+        heapq.heappush(ropes, cur)
+        res += cur
+    return res
+
+ropes = [4, 2, 7, 6, 9]
+print(ConnectRope(ropes))
+
   
   
 }  
